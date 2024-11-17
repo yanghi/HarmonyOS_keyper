8523e16cbec4dc7d00b2689e1cc8d5d013325d13
Yang Hai <yanghi-x@outlook.com>
2024-11-17 16:43:46 +0800

    沉浸式全屏

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -3,9 +3,11 @@ import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 import emitter from '@ohos.events.emitter';
 
+const PREFIX = 'KeyperEntryAbility'
+
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onCreate');
     window.getLastWindow(this.context).then((win) => {
       win.setWindowPrivacyMode(true).then(() => {
         hilog.info(0x0000, 'Keyper', '%{public}s', 'setWindowPrivacyMode=true succeed');
@@ -14,30 +16,81 @@ export default class EntryAbility extends UIAbility {
   }
 
   onDestroy() {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onDestroy');
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onWindowStageCreate');
+    let windowClass: window.Window = null;
 
-    windowStage.loadContent('pages/Lock', (err, data) => {
+
+    const loadContent = () => {
+      windowStage.loadContent('pages/Lock', (err, data) => {
+        if (err.code) {
+          hilog.error(0x0000, PREFIX, 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+
+          return;
+        }
+
+        let navHeightVp = px2vp(navBarHe)
+        AppStorage.SetOrCreate('navHeight', navHeightVp)
+        hilog.info(0x0000, PREFIX, '%{public}s', 'avoidArea navHeight vp' + navHeightVp);
+
+        hilog.info(0x0000, PREFIX, 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+      });
+    }
+
+    let navBarHe = 100
+    windowStage.getMainWindow((err, data) => {
+
       if (err.code) {
-        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        console.error('Failed to obtain the main window. Cause: ' + JSON.stringify(err));
+        loadContent()
         return;
       }
-      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
-    });
+
+      windowClass = data;
+      console.info('Succeeded in obtaining the main window. Data: ' + JSON.stringify(data));
+
+      let avoidArea = data.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM)
+      navBarHe = avoidArea.topRect.height
+      hilog.info(0x0000, PREFIX, '%{public}s', 'avoidArea nav H' + avoidArea.topRect.height);
+
+      windowClass.setWindowSystemBarEnable(['status', 'navigation'], (err) => {
+        if (err.code) {
+          loadContent()
+          console.error('Failed to set the system bar to be visible. Cause:' + JSON.stringify(err));
+          return;
+        }
+        console.info('Succeeded in setting the system bar to be visible.');
+      });
+
+      windowClass.setWindowLayoutFullScreen(true).finally(loadContent)
+    })
+
+    // windowStage.loadContent('pages/Lock', (err, data) => {
+    //   if (err.code) {
+    //     hilog.error(0x0000, TAG, 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+    //     return;
+    //   }
+    //
+    //   let navHeightVp = px2vp(navBarHe)
+    //   AppStorage.SetOrCreate('navHeight', navHeightVp)
+    //   hilog.info(0x0000, TAG, '%{public}s', 'avoidArea navHeight vp' +navHeightVp);
+    //
+    //   hilog.info(0x0000, TAG, 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    // });
   }
 
   onWindowStageDestroy() {
     // Main window is destroyed, release UI related resources
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onWindowStageDestroy');
   }
 
   onForeground() {
     // Ability has brought to foreground
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onForeground');
     emitter.emit({
       eventId: 500
     })
@@ -45,7 +98,7 @@ export default class EntryAbility extends UIAbility {
 
   onBackground() {
     // Ability has back to background
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+    hilog.info(0x0000, PREFIX, '%{public}s', 'Ability onBackground');
     emitter.emit({
       eventId: 501
     })
